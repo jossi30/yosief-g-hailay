4989f164439a068d6cb308ceb86f1cbfe38464ad
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-06-16 20:40:32 +0300

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
+
 function App() {
   return (
     <>
+      
       <Hero />
       <Projects />
       <Skills />
